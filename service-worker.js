9be4975446faa7b53212099cad8994865eada1a0
Rajesh Das Kohli <rajeshdaskohli@gmail.com>
2026-02-09 11:10:38 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,16 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "expense-app-cache-v1";
+
+self.addEventListener("install", event => {
   console.log("Service Worker Installed");
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   console.log("Service Worker Activated");
+  event.waitUntil(clients.claim());
 });
 
-self.addEventListener("fetch", (event) => {
-  // अभी कुछ भी cache मत करो
+self.addEventListener("fetch", event => {
+  // फिलहाल सिर्फ pass-through
+  event.respondWith(fetch(event.request));
 });
