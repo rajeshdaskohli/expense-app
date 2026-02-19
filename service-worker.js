1bc559b12f44ee4e3b95ba8027e357303d995672
Rajesh Das Kohli <rajeshdaskohli@gmail.com>
2026-02-19 23:46:38 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,21 @@
-self.addEventListener("install", () => {
-  console.log("SW Installed");
+const CACHE_NAME="expense-pro-v3";
+
+const urls=[
+"/",
+"/index.html",
+"/css/ui.css",
+"/js/logic.js",
+"/manifest.json"
+];
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache=>cache.addAll(urls))
+);
 });
 
-self.addEventListener("activate", () => {
-  console.log("SW Activated");
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>res||fetch(e.request))
+);
 });
