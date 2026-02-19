0c93c32858f75ef947a3bf0172d2ff1e22e29d85
Rajesh Das Kohli <rajeshdaskohli@gmail.com>
2026-02-19 23:16:46 +0530

    main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
   document.documentElement.setAttribute('data-theme', savedTheme);
 })();
     
-    const API_URL = "https://script.google.com/macros/s/AKfycbyZObRto7KNBBP6zknPF2Lq5ppQWcmeS0fK0gax22KYVrlfTIu-iwuznBT6hdZUDO55vQ/exec";
+    const API_URL = "https://script.google.com/macros/s/AKfycby5ev7nO-ZLCPRrKpySlYVmlptd1CqZFvBRxdx5U1zMV0a2UvqqasAJeLiuKugjFCwcfA/exec";
     let db = null;
     let curPage = 0;
     const fmt = (n) => new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(Number(n) || 0);
