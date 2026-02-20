f6464fdb687efa36ae2ea697790d80f526c0106b
Rajesh Das Kohli <rajeshdaskohli@gmail.com>
2026-02-20 19:45:47 +0530

    logic.js

--- js/logic.js
+++ js/logic.js
@@ -3,7 +3,7 @@
   document.documentElement.setAttribute('data-theme', savedTheme);
 })();
     
-    const API_URL = "https://script.google.com/macros/s/AKfycbzApg87wOrXKf-s7g8147FXsE7sTHYrim_WSQiSSvdwmdTsR26E97gcPC-Leb_q4T0yog/exec";
+    const API_URL = "https://script.google.com/macros/s/AKfycbzxZmlIF-GnMRJcUSFqRmE0D9ibx0Uq-YwasLAIzVXS-wz4wOGrCRHw-QIVTqgaqPcrFQ/exec";
     let db = null;
     let curPage = 0;
     const fmt = (n) => new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' }).format(Number(n) || 0);
